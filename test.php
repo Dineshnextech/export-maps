a17d93cabbc827d2a6b6a5f9306a3c7d227f5335
Dineshnextech <115215556+Dineshnextech@users.noreply.github.com>
2023-02-17 12:32:40 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$_SERVER['REQUEST_URI']; 
+$strs = explode("url=",$_SERVER['REQUEST_URI']);
+$url = $strs[1];
+
+$cmd = 'node generate-full-page-base64.js "'.$url.'"   2>&1';
+$base64 = shell_exec($cmd);
+echo  "<img src=data:image/png;base64,{$img}>";  
+// echo $base64;  
+
+?>
