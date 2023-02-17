c78966c746ded855d23e5d17e455e7f7117856b2
Dineshnextech <115215556+Dineshnextech@users.noreply.github.com>
2023-02-17 12:33:57 +0530

    Update test.php

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ $url = $strs[1];
 
 $cmd = 'node generate-full-page-base64.js "'.$url.'"   2>&1';
 $base64 = shell_exec($cmd);
-echo  "<img src=data:image/png;base64,{$img}>";  
-// echo $base64;  
+// echo  "<img src=data:image/png;base64,{$img}>";  
+echo $base64;  
 
 ?>
